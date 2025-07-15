7cd1602f2f7706afe3cf0b5ace93314ec0e9ff47
Doru Thom Popovici <dorupopovici13@gmail.com>
2025-07-14 21:13:29 -0700

    changes

--- generator/gather_solutions.py
+++ generator/gather_solutions.py
@@ -2,9 +2,9 @@ from pathlib import Path
 import csv
 
 # fft 2D d and di
-def is_fft_2d(item):
+def is_fft_2d(prefix, item):
     if "ft2d" in item.name and "-f_" in item.name and "-B--" in item.name:
-        filename = item.parent.name + "/" + item.name + "/current.sum"
+        filename = prefix + item.parent.name + "/" + item.name + "/current.sum"
         
         with open(filename, 'r') as file:
             for line in file:
@@ -12,7 +12,7 @@ def is_fft_2d(item):
                     return ["fft2D_1D_f_16384_16384", line.split(" ")[-1].rstrip('\n\r')]
 
     if "ft2d" in item.name and "-fb_" in item.name and "-B--" in item.name:
-        filename = item.parent.name + "/" + item.name + "/current.sum"
+        filename = prefix + item.parent.name + "/" + item.name + "/current.sum"
     
         with open(filename, 'r') as file:
             for line in file:
@@ -22,9 +22,9 @@ def is_fft_2d(item):
     return []
 
 # fft 2D batch d and di
-def is_fft_2d_batch(item):
+def is_fft_2d_batch(prefix, item):
     if "ft2d" in item.name and "-f-" in item.name and "-batch_" in item.name and "-B-" in item.name:
-        filename = item.parent.name + "/" + item.name + "/current.sum"
+        filename = prefix + item.parent.name + "/" + item.name + "/current.sum"
         
         with open(filename, 'r') as file:
             for line in file:
@@ -32,7 +32,7 @@ def is_fft_2d_batch(item):
                     return ["fft2D_2D_f_batch_1024_1024_512", line.split(" ")[-1].rstrip('\n\r')]
 
     if "ft2d" in item.name and "-fb-" in item.name and "-batch_" in item.name and "-B-" in item.name:
-        filename = item.parent.name + "/" + item.name + "/current.sum"
+        filename = prefix + item.parent.name + "/" + item.name + "/current.sum"
     
         with open(filename, 'r') as file:
             for line in file:
@@ -40,7 +40,7 @@ def is_fft_2d_batch(item):
                     return ["fft2D_2D_fb_batch_1024_1024_512", line.split(" ")[-1].rstrip('\n\r')]
     
     if "ft2d" in item.name and "-f-" in item.name and "-batch_" in item.name and "-X-" in item.name:
-        filename = item.parent.name + "/" + item.name + "/current.sum"
+        filename = prefix + item.parent.name + "/" + item.name + "/current.sum"
         
         with open(filename, 'r') as file:
             for line in file:
@@ -48,7 +48,7 @@ def is_fft_2d_batch(item):
                     return ["fft2D_2D_f_batch_4096_4096_32", line.split(" ")[-1].rstrip('\n\r')]
 
     if "ft2d" in item.name and "-fb-" in item.name and "-batch_" in item.name and "-X-" in item.name:
-        filename = item.parent.name + "/" + item.name + "/current.sum"
+        filename = prefix + item.parent.name + "/" + item.name + "/current.sum"
     
         with open(filename, 'r') as file:
             for line in file:
@@ -58,9 +58,9 @@ def is_fft_2d_batch(item):
     return []
 
 # fft 2D batch d and di + mm
-def is_fft_2d_mm(item):
+def is_fft_2d_mm(prefix, item):
     if "ft2d" in item.name and "-f-" in item.name and "-mm_" in item.name and "-B-" in item.name:
-        filename = item.parent.name + "/" + item.name + "/current.sum"
+        filename = prefix + item.parent.name + "/" + item.name + "/current.sum"
         
         with open(filename, 'r') as file:
             for line in file:
@@ -68,7 +68,7 @@ def is_fft_2d_mm(item):
                     return ["fft2D_2D_f_batch_gemm_1024_1024_512", line.split(" ")[-1].rstrip('\n\r')]
 
     if "ft2d" in item.name and "-fb-" in item.name and "-mm_" in item.name and "-B-" in item.name:
-        filename = item.parent.name + "/" + item.name + "/current.sum"
+        filename = prefix + item.parent.name + "/" + item.name + "/current.sum"
     
         with open(filename, 'r') as file:
             for line in file:
@@ -76,7 +76,7 @@ def is_fft_2d_mm(item):
                     return ["fft2D_2D_fb_batch_gemm_1024_1024_512", line.split(" ")[-1].rstrip('\n\r')]
     
     if "ft2d" in item.name and "-f-" in item.name and "-mm_" in item.name and "-X-" in item.name:
-        filename = item.parent.name + "/" + item.name + "/current.sum"
+        filename = prefix + item.parent.name + "/" + item.name + "/current.sum"
         
         with open(filename, 'r') as file:
             for line in file:
@@ -84,7 +84,7 @@ def is_fft_2d_mm(item):
                     return ["fft2D_2D_f_batch_gemm_4096_4096_32", line.split(" ")[-1].rstrip('\n\r')]
 
     if "ft2d" in item.name and "-fb-" in item.name and "-mm_" in item.name and "-X-" in item.name:
-        filename = item.parent.name + "/" + item.name + "/current.sum"
+        filename = prefix + item.parent.name + "/" + item.name + "/current.sum"
     
         with open(filename, 'r') as file:
             for line in file:
@@ -94,9 +94,9 @@ def is_fft_2d_mm(item):
     return []
 
 # fft 3D di
-def is_fft_3d(item):
+def is_fft_3d(prefix, item):
     if "ft3d" in item.name and "fb_1D-" in item.name:
-        filename = item.parent.name + "/" + item.name + "/current.sum"
+        filename = prefix + item.parent.name + "/" + item.name + "/current.sum"
         
         with open(filename, 'r') as file:
             for line in file:
@@ -104,7 +104,7 @@ def is_fft_3d(item):
                     return ["fft3D_1D_fb_256_256_256", line.split(" ")[-1].rstrip('\n\r')]
 
     if "ft3d" in item.name and "fb_2D-" in item.name:
-        filename = item.parent.name + "/" + item.name + "/current.sum"
+        filename = prefix + item.parent.name + "/" + item.name + "/current.sum"
     
         with open(filename, 'r') as file:
             for line in file:
@@ -114,9 +114,9 @@ def is_fft_3d(item):
     return []
 
 # fft 3D batch di
-def is_fft_3d_batch(item):
+def is_fft_3d_batch(prefix, item):
     if "ft3d" in item.name and "fb-batch_2D-" in item.name:
-        filename = item.parent.name + "/" + item.name + "/current.sum"
+        filename = prefix + item.parent.name + "/" + item.name + "/current.sum"
         
         with open(filename, 'r') as file:
             for line in file:
@@ -124,7 +124,7 @@ def is_fft_3d_batch(item):
                     return ["fft3D_2D_fb_batch_256_256_256_1024", line.split(" ")[-1].rstrip('\n\r')]
 
     if "ft3d" in item.name and "fb-batch_3D-" in item.name:
-        filename = item.parent.name + "/" + item.name + "/current.sum"
+        filename = prefix + item.parent.name + "/" + item.name + "/current.sum"
     
         with open(filename, 'r') as file:
             for line in file:
@@ -134,9 +134,9 @@ def is_fft_3d_batch(item):
     return []
 
 # fft 3D batch di + mm
-def is_fft_3d_mm(item):
+def is_fft_3d_mm(prefix, item):
     if "ft3d" in item.name and "fb-mm_2D-" in item.name:
-        filename = item.parent.name + "/" + item.name + "/current.sum"
+        filename = prefix + item.parent.name + "/" + item.name + "/current.sum"
         
         with open(filename, 'r') as file:
             for line in file:
@@ -144,7 +144,7 @@ def is_fft_3d_mm(item):
                     return ["fft3D_2D_fb_batch_gemm_256_256_256_1024", line.split(" ")[-1].rstrip('\n\r')]
 
     if "ft3d" in item.name and "fb-mm_3D-" in item.name:
-        filename = item.parent.name + "/" + item.name + "/current.sum"
+        filename = prefix + item.parent.name + "/" + item.name + "/current.sum"
     
         with open(filename, 'r') as file:
             for line in file:
@@ -153,7 +153,7 @@ def is_fft_3d_mm(item):
     
     return []
 
-folder_path = Path("../solver/solutions/") # Replace with your folder's path
+folder_path = Path("../solver/regenerated/") # Replace with your folder's path
 
 fft_2D_data = []
 fft_2D_batch_data = []
@@ -164,50 +164,50 @@ fft_3D_batch_data = []
 fft_3D_mm_data = []
 
 for item in folder_path.iterdir():
-    result = is_fft_2d(item)
+    result = is_fft_2d("../solver/", item)
     if(len(result) != 0):
         fft_2D_data.append(result)
         
-    result = is_fft_2d_batch(item)
+    result = is_fft_2d_batch("../solver/", item)
     if(len(result) != 0):
         fft_2D_batch_data.append(result)
     
-    result = is_fft_2d_mm(item)
+    result = is_fft_2d_mm("../solver/", item)
     if(len(result) != 0):
         fft_2D_mm_data.append(result)
         
-    result = is_fft_3d(item)
+    result = is_fft_3d("../solver/", item)
     if(len(result) != 0):
         fft_3D_data.append(result)
         
-    result = is_fft_3d_batch(item)
+    result = is_fft_3d_batch("../solver/", item)
     if(len(result) != 0):
         fft_3D_batch_data.append(result)
     
-    result = is_fft_3d_mm(item)
+    result = is_fft_3d_mm("../solver/", item)
     if(len(result) != 0):
         fft_3D_mm_data.append(result)
 
-with open("fft_2D/result_new.csv", 'w', newline='') as file:
+with open("fft_2D/results.csv", 'w', newline='') as file:
     writer = csv.writer(file, delimiter='|') 
     writer.writerows(fft_2D_data)
     
-with open("fft_2D_batch/result_new.csv", 'w', newline='') as file:
+with open("fft_2D_batch/results.csv", 'w', newline='') as file:
     writer = csv.writer(file, delimiter='|') 
     writer.writerows(fft_2D_batch_data)
     
-with open("fft_2D_batch_mm/result_new.csv", 'w', newline='') as file:
+with open("fft_2D_batch_mm/results.csv", 'w', newline='') as file:
     writer = csv.writer(file, delimiter='|') 
     writer.writerows(fft_2D_mm_data)
     
-with open("fft_3D/result_new.csv", 'w', newline='') as file:
+with open("fft_3D/results.csv", 'w', newline='') as file:
     writer = csv.writer(file, delimiter='|') 
     writer.writerows(fft_3D_data)
     
-with open("fft_3D_batch/result_new.csv", 'w', newline='') as file:
+with open("fft_3D_batch/results.csv", 'w', newline='') as file:
     writer = csv.writer(file, delimiter='|') 
     writer.writerows(fft_3D_batch_data)
     
-with open("fft_3D_batch_mm/result_new.csv", 'w', newline='') as file:
+with open("fft_3D_batch_mm/results.csv", 'w', newline='') as file:
     writer = csv.writer(file, delimiter='|') 
     writer.writerows(fft_3D_mm_data)
